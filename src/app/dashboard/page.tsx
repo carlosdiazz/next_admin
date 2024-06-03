1344e657ca294725892cc0ea7158c8cf7d2f13f7
Carlos DIaz <c.diazadriann@gmail.com>
2024-06-03 17:42:17 -0400

    Agregando Auth

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,23 @@
 import { WidgetItem } from "@/components";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../api/auth/[...nextauth]/route";
+import { redirect } from "next/navigation";
 
-export default function DashboardPage() {
+export default async function DashboardPage() {
+
+  const session = await getServerSession(authOptions)
+  if (!session) {
+    redirect('/api/auth/signin');
+  }
+
   return (
-    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
-      <WidgetItem title="Prueba">
-        <span>Qloq</span>
+    <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 ">
+      <WidgetItem title="Usuario Conectado S-side">
+        <div className="flex flex-col">
+          <span>Nombre: {session.user?.name}</span>
+          <span>Imagen: {session.user?.image}</span>
+          <span>Correo: {session.user?.email }</span>
+        </div>
       </WidgetItem>
     </div>
   );
